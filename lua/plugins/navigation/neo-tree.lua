0759b0508336a07bc23199fade63157e0a067661
Aleks Tacconi <aleks.tacconi@gmail.com>
2025-06-03 20:39:53 +0000

    change neo-tree.nvim

--- lua/plugins/navigation/neo-tree.lua
+++ lua/plugins/navigation/neo-tree.lua
@@ -10,19 +10,6 @@ return {
 
 	config = function()
 		local neotree = require("neo-tree")
-		local show_hidden = false
-
-		local function is_neotree_open()
-			for _, win in ipairs(vim.api.nvim_list_wins()) do
-				local buf = vim.api.nvim_win_get_buf(win)
-				local name = vim.api.nvim_buf_get_name(buf)
-
-				if name:match("neo%-tree") then
-					return true
-				end
-			end
-			return false
-		end
 
 		neotree.setup({
 			event_handlers = {
@@ -33,29 +20,13 @@ return {
 					end,
 				},
 			},
-			filesystem = {
-				filtered_items = {
-					visible = show_hidden,
-					hide_dotfiles = not show_hidden,
-					hide_hidden = not show_hidden,
-				},
-			},
 		})
 
-		vim.keymap.set("n", "<Leader>d", function()
-			if is_neotree_open() then
-				show_hidden = not show_hidden
-				neotree.setup({
-					filesystem = {
-						filtered_items = {
-							visible = show_hidden,
-							hide_dotfiles = not show_hidden,
-							hide_hidden = not show_hidden,
-						},
-					},
-				})
-			end
+		vim.keymap.set("n", "<Leader>m", function()
+            vim.cmd("Neotree reveal")
+		end)
 
+		vim.keymap.set("n", "<Leader>d", function()
 			vim.cmd("Neotree filesystem reveal left")
 		end)
 	end,
