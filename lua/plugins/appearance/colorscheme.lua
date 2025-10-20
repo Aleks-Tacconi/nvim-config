7d51d31f9ece794ef644c6790961ddd756741d69
Aleks Tacconi <aleks.tacconi@gmail.com>
2025-10-20 16:41:23 +0100

    improve color scheme

--- lua/plugins/appearance/colorscheme.lua
+++ lua/plugins/appearance/colorscheme.lua
@@ -11,7 +11,7 @@ return {
         vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
         vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
         vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-        vim.api.nvim_set_hl(0, "FloatBorder", { bg = "none" })
+        vim.api.nvim_set_hl(0, "FloatBorder", { bg = "none", fg="grey" })
         vim.api.nvim_set_hl(0, "VertSplit", { bg = "none" })
         vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
         vim.api.nvim_set_hl(0, "StatusLine", { bg = "none" })
