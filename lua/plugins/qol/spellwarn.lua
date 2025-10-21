af8135ee0970caeaa8efe149a4ca09ef34be2bca
Aleks Tacconi <aleks.tacconi@gmail.com>
2025-10-21 12:50:58 +0100

    fix linter issues

--- lua/plugins/qol/spellwarn.lua
+++ lua/plugins/qol/spellwarn.lua
@@ -1,5 +1,31 @@
 return {
     "ravibrock/spellwarn.nvim",
     event = "VeryLazy",
-    config = true,
+    config = function()
+        require("spellwarn").setup({
+
+            event = {
+                "InsertLeave",
+                "TextChanged",
+                "TextChangedI",
+                "TextChangedP",
+            },
+            enable = true,
+            ft_config = {
+                markdown = true,
+                txt = true,
+            },
+            ft_default = false,
+            max_file_size = nil,
+            severity = {
+                spellbad = "WARN",
+                spellcap = "HINT",
+                spelllocal = "HINT",
+                spellrare = "INFO",
+            },
+            suggest = false,
+            prefix = "[spellwarn] ",
+            diagnostic_opts = { severity_sort = true },
+        })
+    end,
 }
