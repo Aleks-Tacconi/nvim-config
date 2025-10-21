82b21aefc3fc53145c29599e3bbe09f3553665d9
Aleks Tacconi <aleks.tacconi@gmail.com>
2025-10-21 22:21:05 +0100

    java

--- lua/lsp/config/keymaps.lua
+++ lua/lsp/config/keymaps.lua
@@ -15,7 +15,7 @@ function M.on_attach(_, bufnr)
     vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = bufnr })
     vim.keymap.set("n", "gi", builtin.lsp_implementations, { buffer = bufnr })
     vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { buffer = bufnr })
-    vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { buffer = bufnr })
+    -- vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { buffer = bufnr })
     vim.keymap.set("n", "<leader>ca", require("actions-preview").code_actions, { buffer = bufnr })
     vim.keymap.set("n", "<leader>p", open_float, { buffer = bufnr })
 end
