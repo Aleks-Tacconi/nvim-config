3c6d71582ae678485f13030ea6c90875bbeaff2f
Aleks Tacconi <aleks.tacconi@gmail.com>
2025-10-27 13:25:14 +0000

    neotest-python

--- lua/plugins/qol/neotest.lua
+++ lua/plugins/qol/neotest.lua
@@ -4,18 +4,19 @@ return {
 	"nvim-neotest/neotest",
 	dependencies = {
 		"nvim-neotest/nvim-nio",
+		"nvim-neotest/neotest-python",
 		"nvim-lua/plenary.nvim",
 		"antoinemadec/FixCursorHold.nvim",
 		"nvim-treesitter/nvim-treesitter",
 	},
 	config = function()
-		-- require("neotest").setup({
-		-- 	adapters = {
-		-- 		require("neotest-python")({
-		-- 			python = utils.get_path("python"),
-		-- 		}),
-		-- 		require("neotest-java")({}),
-		-- 	},
-		-- })
+		require("neotest").setup({
+			adapters = {
+				require("neotest-python")({
+					python = utils.get_path("python"),
+				}),
+				-- require("neotest-java")({}),
+			},
+		})
 	end,
 }
