f6fa41631b39f1f1a773fa23a232d9011d45115a
Aleks Tacconi <aleks.tacconi@gmail.com>
2026-04-03 13:04:29 +0100

    Add test.lua for testing lazy install of github.com/Aleks-Tacconi/rtfm.nvim

---
+++ lua/plugins/qol/test.lua
@@ -0,0 +1,20 @@
+return {
+-- 	"Aleks-Tacconi/rtfm.nvim",
+-- 	dependencies = {
+-- 		"nvim-telescope/telescope.nvim",
+-- 	},
+-- 	config = function()
+-- 		require("rtfm").setup({
+-- 			keymaps = {
+-- 				manage = "<leader>rm",
+-- 				browse = "<leader>rb",
+-- 			},
+-- 			viewer = {
+-- 				keymaps = {
+-- 					prev = "[d",
+-- 					next = "]d",
+-- 				},
+-- 			},
+-- 		})
+-- 	end,
+}
